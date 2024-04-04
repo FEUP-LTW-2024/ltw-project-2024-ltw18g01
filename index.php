ae1de25478d00675d290252361daf34902c011c5
Clara Sousa <clarasousa2004@hotmail.com>
2024-04-04 21:57:34 +0100

    navbar

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+        
+        <title>Techie</title>
+
+        <link rel="stylesheet" href="index_style.css"> 
+   </head>
+    <body>
+        <div id="navbar-text" class="navbar">
+            <a class="active" href="#home">Logo</a>
+            <a href="#search">Search</a>
+            <a href="#sell">Sell</a>
+            <a href="#message">Message</a>
+            <a href="#account">Account</a>
+        </div>
+    </body>
+</html>
