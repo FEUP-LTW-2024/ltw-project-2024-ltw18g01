493b03f6e3666d59d366d626955a755c23e647fe
Clara Sousa <clarasousa2004@hotmail.com>
2024-04-11 17:53:44 +0100

    metade da home page já está

--- index.php
+++ index.php
@@ -10,12 +10,34 @@
         <link rel="stylesheet" href="index_style.css"> 
    </head>
     <body>
-        <div id="navbar-text" class="navbar">
-            <a class="active" href="#home">Logo</a>
-            <a href="#search">Search</a>
-            <a href="#sell">Sell</a>
-            <a href="#message">Message</a>
-            <a href="#account">Account</a>
-        </div>
+        <img class="logo" src="images/logo/Techie_logo.png" /> 
+        <header  id="navbar-text" class="navbar">
+            <a class="active" href="#home">Home</a>
+            <a href="#Gaming">Gaming</a>
+            <a href="#PCs">PC's</a>
+            <a href="#Mobiles">Mobiles</a>
+            <a href="#TVs">TV's</a>
+            <a href="#Music">Music</a>
+            <a href="#Photo&Video">Photo&Video</a>
+        </header>
+        <!-- Section of quotes-->
+        <section>
+            <div class="info-text">
+                <p id="First-text">Fancy some pre-loved tech?</p>
+                <p id="Second-text">Buy and sell at Techie. Zero fees*. Zero hassle.</p>
+                <p id="disclaimer" >*Buyer pays for the shipping.</p>
+            </div>
+        </section>
+        <!-- New Arrivals-->
+        <section>
+            <div class="displays">
+                <p id="category">New arrivals</p>
+            </div>
+            <div id="image_display">
+                <img src="images/products/macintoshplus.jpg">
+                <img src="images/products/commodore64.jpg">
+                <img src="images/products/ZXSpectrum48k.jpg">
+            </div>
+        </section>
     </body>
 </html>
