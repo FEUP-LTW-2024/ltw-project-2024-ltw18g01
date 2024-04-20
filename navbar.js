3be6e6f52aac001b5f3d04df5f34357b0c46568e
Clara Sousa <clarasousa2004@hotmail.com>
2024-04-20 20:27:58 +0100

    diferenciação entre mobile e desktop

--- navbar.js
+++ navbar.js
@@ -2,7 +2,6 @@ function toggleDesktopMenu() {
     var desktopMenu = document.querySelector('.desktop-menu');
     desktopMenu.classList.toggle('show');
 
-    // Mostrar/ocultar a barra de navegação das categorias ao clicar no ícone do menu móvel
     var categoriesNavbar = document.querySelector('.categories-navbar');
     categoriesNavbar.style.display = (categoriesNavbar.style.display === "block") ? "none" : "block";
 }
