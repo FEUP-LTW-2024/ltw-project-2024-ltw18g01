b0b71b5b7be279d0995219e24deaefb809a37f36
João Vicente <up202208586@up.pt>
2024-04-28 17:56:00 +0100

    Login logic works

--- db/user.class.php
+++ db/user.class.php
@@ -50,7 +50,8 @@
               WHERE lower(email) = ? AND password = ?
             ');
       
-            $stmt->execute(array(strtolower($email), sha1($password)));
+            // $stmt->execute(array(strtolower($email), sha1($password)));
+            $stmt->execute(array(strtolower($email), $password));
         
             if ($user = $stmt->fetch()) {
               return new User(
