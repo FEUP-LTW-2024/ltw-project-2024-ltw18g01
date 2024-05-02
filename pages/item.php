e940cf846f7c66813dfe0bc6e465813b57b6b8e6
João Vicente <up202208586@up.pt>
2024-05-02 12:38:13 +0100

    item page (beginning)

---
+++ pages/item.php
@@ -0,0 +1,47 @@
+<?php
+  declare(strict_types = 1);
+
+  require_once(__DIR__ . '/../sessions/session.php');
+  $session = new Session();
+
+  require_once(__DIR__ . '/../db/connection.db.php');
+  require_once(__DIR__ . '/../db/user.class.php');
+  require_once(__DIR__ . '/../templates/common.tpl.php');
+
+  $db = databaseConnect();
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+   <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+        
+        <title>Techie</title>
+
+        <link rel="stylesheet" href="/css/index_style.css">
+        <link rel="stylesheet" href="/css/item.css"> 
+   </head>
+    <body>
+        <?php
+         drawTopBar($session, $db);
+        ?>
+        
+        <br>
+
+        <section>
+            <div class="background">
+                <div class="item_picture">
+                    <img src="/images/products/macintoshplus.jpg" alt="Item Picture">
+                </div>
+            </div>
+        </section>
+
+
+    </body>
+
+
+</html>
