abdf81181a37d6ebfde654ed50c45ab6cec17850
João Vicente <up202208586@up.pt>
2024-05-19 22:47:02 +0100

    guest homepage fix

--- templates/category.tpl.php
+++ templates/category.tpl.php
@@ -15,7 +15,6 @@
 
 <?php
 function drawCategorySlideHomePage(Category $cat, PDO $db, int $userId) {
-    // Retrieve items for the given category
     $items = Category::getCategoryItems($db, $cat->id);
     $wishlistItemIds = Wishlist::getWishlistUserIDs($db, $userId);
     $count = 0;
@@ -56,47 +55,40 @@ function drawCategorySlideHomePage(Category $cat, PDO $db, int $userId) {
 
 <?php
 function drawCategorySlideGuestHomePage(Category $cat, PDO $db) {
-    // Retrieve items for the given category
     $items = Category::getCategoryItems($db, $cat->id);
     $count = 0;
-?>  
-    <div class="image_display">
-        <?php foreach ($items as $item) { ?>
-            <?php
-            if ($count >= 8) {
-                break;
-            }
-            ?>
-            <?php
-            if ($item['sold'] == false) { ?>
-            <?php $count++; ?>
-            <div class="image_wrapper">
-                <a href="/../pages/item.php?itemId=<?=$item['itemId']?>">
-                <img src="<?php echo $item['image_url']; ?>">
-                </a>
-                <p><?php echo $item['price'] . "€  | " . $item['likes'] . " likes"; ?></p>
-                
-                <form method="POST" action="/../actions/like_action.php">
-                    <input type="hidden" name="itemId" value="<?php echo $item['itemId']; ?>">
-                    <button></button>
-                </form>
-                
-                <!-- ajax ver.
-              button class="like-button" data-user="<?php/* echo $session->getId(); */?>" data-item="<?php/* echo $wlitem['itemId']; */?>"></button>
-                <script src="/../js/wishlist.js"></script>
-                --->
-            </div>
-            <?php } ?>
-        <?php } ?>
-    </div>
-<?php 
-} 
+?>      
+        <div class="displays">
+            <p class="category"><?php echo $cat->name; ?></p>
+            <a class="see_more" href=<?php echo "/pages/category.php?category=" . $cat->id . "&subcategory=all";?>><p>See more</p></a>
+        </div>
+        <div class="image_display">
+            <?php foreach ($items as $item): ?>
+                <?php if ($count >= 8) break; ?>
+                <?php if (!$item['sold']): ?>
+                    <?php $count++; ?>
+                    <div class="image_wrapper">
+                        <a href="/../pages/item.php?itemId=<?= $item['itemId'] ?>">
+                            <img src="<?= $item['image_url'] ?>" alt="Item Image">
+                        </a>
+                        <p><?= $item['price'] . "€  | " . $item['likes'] . " likes"; ?></p>
+                        
+                        <form method="POST" action="/../actions/like_action.php">
+                            <input type="hidden" name="itemId" value="<?= $item['itemId'] ?>">
+
+                            <button></button>
+                        </form>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+<?php
+}
 ?>
 
 
 <?php
 function drawCategorySlide(Category $cat, PDO $db, int $userId) {
-    // Retrieve items for the given category
     $items = Category::getCategoryItems($db, $cat->id);
     $wishlistItemIds = Wishlist::getWishlistUserIDs($db, $userId);
 ?>  
@@ -131,7 +123,6 @@ function drawCategorySlide(Category $cat, PDO $db, int $userId) {
 
 <?php
 function drawCategorySlideGuest(Category $cat, PDO $db) {
-    // Retrieve items for the given category
     $items = Category::getCategoryItems($db, $cat->id);
 ?>  
     <div class="image_display">
@@ -195,7 +186,6 @@ function drawSubSlide(Subcategory $cat, PDO $db, int $userId) {
 
 <?php
 function drawSubSlideGuest(Subcategory $cat, PDO $db) {
-    // Retrieve items for the given category
     $items = Subcategory::getSubcategoryItems($db, $cat->id);
 ?>  
     <div class="image_display">
