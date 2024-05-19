9a6bf11110318dc9142e0a9208d4b9bfed85800e
Clara Sousa <clarasousa2004@hotmail.com>
2024-05-19 11:39:51 +0100

    criar conversa

--- actions/sendMessage_action.php
+++ actions/sendMessage_action.php
@@ -10,7 +10,16 @@ $itemId = (int)$_POST['itemId'];
 $messageText = $_POST['message'];
 $sentAt = date('Y-m-d H:i:s');
 
-$message = new Message(null, $senderId, $receiverId, $itemId, $messageText, $sentAt);
+$stmt = $db->prepare('SELECT * FROM Message WHERE senderId = ? AND receiverId = ? AND itemId = ?');
+$stmt->execute([$senderId, $receiverId, $itemId]);
+$existingConversation = $stmt->fetch();
+
+if (!$existingConversation) {
+    $message = new Message(null, $senderId, $receiverId, $itemId, $messageText, $sentAt);
+} else {
+    $message = new Message($existingConversation['messageId'], $senderId, $receiverId, $itemId, $messageText, $sentAt);
+}
+
 $messageId = $message->save($db);
 
 if ($messageId) {
--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -130,7 +130,6 @@ function drawMyItems($db, $userId) {
     }
 }
 ?>
-
 <?php
 function drawItems($item, $seller, $itemId, $isAdmin, $curUser) { ?>
     <div class="background">
@@ -194,13 +193,8 @@ function drawItems($item, $seller, $itemId, $isAdmin, $curUser) { ?>
                             <input type="hidden" name="itemId" value="<?php echo $itemId; ?>" ?>">
                         </form>
                     <?php } ?>
-
-
                 </div>
-                
-
             </div>
-
         </div>
     </div>
     <script>
